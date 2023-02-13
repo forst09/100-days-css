ae83da51564c6b18cd7a0719a36174d18c9b4716
forst09 <dforst09@gmail.com>
2023-02-13 19:11:41 +0300

    change js 2/100

--- 2 day/script.js
+++ 2 day/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     let burger = document.getElementById('burger');
-    burger.addEventListener('click', function () {
-        this.classList.toggle('active');
-    });
+    if (burger) {
+        burger.addEventListener('click', function () {
+            this.classList.toggle('active');
+        });
+    }
 }); 
