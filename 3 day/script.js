f2a97ee5ce4281760fce5d8a3f4123b68723e45b
forst09 <dforst09@gmail.com>
2023-06-06 14:35:55 +0300

    finish 4 day

--- 3 day/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let burger = document.getElementById('burger');
-    if (burger) {
-        burger.addEventListener('click', function () {
-            this.classList.toggle('active');
-        });
-    }
-}); 
