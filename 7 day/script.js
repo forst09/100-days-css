f6becceaf9705a175c028b9009b1a66c462a4867
daria <dforst09@gmail.com>
2023-08-06 16:29:11 +0300

    cont 7 day

--- 7 day/script.js
+++ 7 day/script.js
@@ -3,19 +3,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuBtn = document.querySelector('#menuBtn');
     const searchBtn = document.querySelector('#searchBtn');
-    function addActive(btn, element) {
+    function addActive(btn, element, element2) {
         if (btn) {
             btn.addEventListener('click', function () {
-                if (document.querySelector(`${element}`).classList.contains('active')) {
-                    document.querySelector(`${element}`).classList.remove('active');
-                }
-                else {
-                    document.querySelector(`${element}`).classList.add('active');
+                document.querySelector(`${element}`).classList.toggle('active');
+                if (element2) {
+                    document.querySelector(`${element2}`).classList.toggle('moved');
                 }
             })
         }
     }
-    addActive(menuBtn, '.menu');
+    addActive(menuBtn, '.menu', '.notifications');
     addActive(searchBtn, '.notifications__search-input');
 
 });
